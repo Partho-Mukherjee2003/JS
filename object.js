53a531b712a6062176eb84bc0ffd001593bc1a6c
Partho-Mukherjee2003 <parthomukherjee582@gmail.com>
2025-02-16 14:19:11 +0800

    add objects sections

--- object.js
+++ object.js
@@ -5,8 +5,51 @@
 //     price: 45,
 //     color:'blue',
 //     isClean:false,
-//     availableColor:['red', 'white', 'blue', 'black']
+//     availableColor:['red', 'white', 'blue', 'black'],
+//     unique: {
+//         color: 'red',
+//         result:{
+//             gpa: 5.00,
+//             merit: 'top',
+//             availableColor:['red', 'white', 'blue', 'black'],
+//         }
+//     }
 // }
 // console.log(bottle);
 // console.log(bottle.color); //Dot notation
 // console.log(bottle['brand'])// Bracket notation
+// const keys = Object.keys(bottle);
+// console.log(keys);
+
+// const values = Object.values(bottle);
+// console.log(values);
+
+// console.log(bottle);
+// delete bottle.color;
+// console.log(bottle);
+// bottle.unique.result.availableColor[2] = 'green';
+// bottle.unique.result['gpa']= 4.56;
+// console.log(bottle.unique.result["gpa"]);
+// console.log(bottle.unique.result.availableColor);
+
+
+// for of--- Array
+// for in---- Object
+
+// for (let bot in bottle){
+//     console.log(bot);
+//     console.log(bottle[bot]);
+// }
+// const keys = Object.keys(bottle);
+// console.log(keys);
+// for(const key of keys){
+    // console.log(key);
+//     console.log(key,':', bottle[key]);
+// }
+
+// const pen = {brand:'econo', price:20, color:'green'}
+// const pencil = new Object();
+// console.log(pencil);
+// const rubber = Object.create({});
+// console.log(rubber);
+
